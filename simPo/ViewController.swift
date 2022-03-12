a2340a2dcc21e50dfa841782b7b1f64f465cb735
Riley Lai <dakimi07@gmail.com>
2022-03-12 14:07:50 +0800

    Setup View

--- simPo/ViewController.swift
+++ simPo/ViewController.swift
@@ -17,3 +17,10 @@ class ViewController: UIViewController {
 
 }
 
+/*
+ 1. 為什麼ViewController的Button設定content insets後，各邊數值都設定相同，但bottom看起來就是特別多（暫時用提高top數值來解決）
+ 2. 為什麼ViewController的背景Image和Button的RGBO設定相同，但實際看起來卻不同？
+ 3. 想要對Button製作Highlighted效果，只能用Image來替換，不能直接用Background嗎？
+ 4. 如何對Label增加padding？
+ 5. 設定segue以後，作為背景的Image大小自動變小？
+ */
