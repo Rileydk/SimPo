6ae9985d3829d782fedcf5c3692d5bd57690ea30
Riley Lai <dakimi07@gmail.com>
2022-03-13 08:50:05 +0800

    Adjust: Substitute background for image

--- simPo/SetTimerViewController.swift
+++ simPo/SetTimerViewController.swift
@@ -18,12 +18,13 @@ class SetTimerViewController: UIViewController {
     statusLabel.text = status
     if status == "Focus" {
       print("status is focus")
-      self.view.backgroundColor = UIColor(red: 32/255, green: 191/255, blue: 166/255, alpha: 0.85)
+      self.view.backgroundColor = UIColor(red: 32/255, green: 191/255, blue: 166/255, alpha: 1)
     } else if status == "Rest" {
       print("status is Rest")
       self.view.backgroundColor = UIColor(red: 245/255, green: 208/255, blue: 1/255, alpha: 1)
     }
   }
+  
   @IBAction func backToFrontPage() {
     dismiss(animated: true, completion: nil)
   }
